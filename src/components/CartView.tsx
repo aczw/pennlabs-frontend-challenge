adb765de118ecf15c2ac035045943fbcda4babc8
Charles Wang <czw@seas.upenn.edu>
2023-09-17 20:20:51 -0400

    okay actually prep

--- src/components/CartView.tsx
+++ src/components/CartView.tsx
@@ -1,11 +1,32 @@
 import type { Course } from "~/utils/types";
 
 const CartView = ({ cart }: { cart: Course[] }) => {
+  const cartEmpty = cart.length === 0;
+
   return (
     <div className="flex justify-end gap-3">
-      <div className="flex w-full flex-col md:w-1/2">
-        <h3>Your cart</h3>
-        <div className="rounded-md bg-gray-200 p-4">{cart.length}</div>
+      <div className="grid w-full grid-cols-1 gap-5 md:grid-cols-2">
+        <div className="flex flex-col items-end space-y-2">
+          <h3 className="text-xl font-semibold leading-none">Current cart</h3>
+          <p>
+            When you feel ready, head over to checkout to finalize your courses.
+          </p>
+          <button
+            disabled={cartEmpty}
+            className="w-[300px] rounded-full bg-gray-500 py-1.5 font-semibold text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-gray-500"
+          >
+            Checkout!
+          </button>
+        </div>
+        {cartEmpty ? (
+          <div className="flex items-center justify-center rounded-md bg-gray-200 p-4 text-gray-500">
+            Currently empty! Add some courses from below to checkout.
+          </div>
+        ) : (
+          <div className="flex rounded-md bg-gray-200 p-4 text-gray-500">
+            Some stuff idk
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,39 +6,45 @@ const Header = ({ cart }: { cart: Course[] }) => {
   const [cartOpen, setCartOpen] = useState(false);
 
   return (
-    <header className="fixed flex w-full flex-col space-y-4 border-b border-black/10 bg-gray-50 px-8 py-3">
-      <div className="flex justify-between">
-        <nav className="flex items-center space-x-6">
-          <a
-            href="/#"
-            className="flex items-center space-x-2 hover:underline"
+    <div className="fixed w-full">
+      <header className="flex flex-col space-y-4 border-b border-black/10 bg-gray-50 px-8 py-3">
+        <div className="flex justify-between">
+          <nav className="flex items-center space-x-6">
+            <a
+              href="/#"
+              className="flex items-center space-x-2 hover:underline"
+            >
+              <img
+                src="/logo.png"
+                width="23"
+              />
+              <h2 className="align-top text-xl font-semibold">
+                Penn Course Cart
+              </h2>
+            </a>
+            <a
+              href="https://github.com/aczw"
+              className="text-zinc-700 hover:underline"
+              target="_blank"
+            >
+              @aczw
+            </a>
+          </nav>
+          <button
+            id="sex"
+            className="rounded-full bg-sky-500 px-3 py-1.5 font-bold text-white hover:bg-sky-600 hover:underline"
+            onClick={() => setCartOpen(!cartOpen)}
           >
-            <img
-              src="/logo.png"
-              width="23"
-            />
-            <h2 className="align-top text-xl font-semibold">
-              Penn Course Cart
-            </h2>
-          </a>
-          <a
-            href="https://github.com/aczw"
-            className="text-zinc-700 hover:underline"
-            target="_blank"
-          >
-            @aczw
-          </a>
-        </nav>
-        <button
-          id="sex"
-          className="rounded-full bg-sky-500 px-3 py-1.5 font-bold text-white hover:bg-sky-600 hover:underline"
-          onClick={() => setCartOpen(!cartOpen)}
-        >
-          Cart ({cart.length})
-        </button>
-      </div>
-      {cartOpen ? <CartView cart={cart} /> : null}
-    </header>
+            Your cart ({cart.length})
+          </button>
+        </div>
+      </header>
+      {cartOpen ? (
+        <div className="border-b border-black/10 bg-gray-100 px-8 py-5 drop-shadow">
+          <CartView cart={cart} />
+        </div>
+      ) : null}
+    </div>
   );
 };
 
