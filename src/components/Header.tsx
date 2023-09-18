1e3f630ddf2d463a239eb33cbf85c67a919f987a
Charles Wang <czw@seas.upenn.edu>
2023-09-18 17:56:01 -0400

    fix the header (oops)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,15 @@
-const Header = ({
-  children,
-  className,
-}: {
-  children?: React.ReactNode;
-  className?: string;
-}) => {
+const Header = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <header
-      className={`flex flex-col space-y-4 border-b border-black/10 bg-gray-50 px-8 py-3 ${
-        className ? className : ""
-      }`}
-    >
+    <header className="flex flex-col space-y-4 border-b border-black/10 bg-gray-50 px-8 py-3">
       <div className="flex items-center justify-between">
-        <nav className="flex items-center space-x-6">
-          <a
-            href="/#"
-            className="flex items-center space-x-2 hover:underline"
-          >
-            <img
-              src="/logo.png"
-              width="32"
-            />
-            <h2 className="align-top text-2xl font-semibold">
-              Penn Course Cart
-            </h2>
-          </a>
-          <a
-            href="https://github.com/aczw"
-            className="text-zinc-700 hover:underline"
-            target="_blank"
-          >
-            @aczw
-          </a>
-        </nav>
+        <div className="flex items-center space-x-3">
+          <img
+            src="/logo.png"
+            width="32"
+          />
+          <h2 className="align-top text-2xl font-semibold">Penn Course Cart</h2>
+        </div>
+
         {children ? children : null}
       </div>
     </header>
